e0de87b6f81b096cda62eb209a3a68ce201fb539
ICT <brandongrey1324@outlook.com>
2025-06-07 18:51:10 -0500

    06_Revert_to_Initial_for_ref

--- build/electron-builder-base.js
+++ build/electron-builder-base.js
@@ -1,40 +1,7 @@
 const config = {
   appId: 'com.evotrade.wallet',
   productName: 'EvoTradeWallet',
-  files: [
-    'compiled/**/*',
-    'bundle/**/*',
-    '!compiled/main/dev/**',
- //   "dist/**/*",
-      "node_modules/**/*",
- //     "package.json",
-      "!node_modules/**/build/node_gyp_bins/**",
-      "!node_modules/**/*.{c,mk,a,o,h,md,gz,txt,markdown,Makefile}",
-      "!node_modules/**/test/**",
-      "!node_modules/**/tests/**",
-      "!node_modules/**/doc/**",
-      "!node_modules/**/docs/**",
-      "!node_modules/**/*.d.ts",
-      "!**/*.map"
-  ],
-  //asar: false,
-  mac: {
-    category: "public.app-category.finance",
-    target: [
-      { target: "mas", arch: "universal" },
-      //{ target: "mas", arch: "arm64" }
-    ],
-    asarUnpack: [
-        "**/*.node",
-        "**/node_modules/{nodegit,sqlite3}/**"
-      ],
-    hardenedRuntime: true,
-    //mergeASARs: false,
-    entitlements: "entitlements.mas.plist",
-    entitlementsInherit: "entitlements.mas.inherit.plist",
-    gatekeeperAssess: true,
-    sandbox: true
-  }
+  files: ['compiled', 'bundle', '!compiled/main/dev']
 }
 
 module.exports = config
